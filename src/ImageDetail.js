a9214c7c16dd488e0aad93a4e31902d05cf68a69
iyokurot <01kayochin17neko@gmail.com>
2019-05-23 06:12:42 +0900

    5/23 upload

--- src/ImageDetail.js
+++ src/ImageDetail.js
@@ -10,7 +10,9 @@ class ImageDetail extends Component {
         this.state = {
             imageid: id,
             file: [],
-            tags: []
+            defaultTags: [],
+            tags: [],
+            newtag: ""
         };
 
         const data = {
@@ -26,6 +28,10 @@ class ImageDetail extends Component {
         })
             .then(response => response.json())
             .then(posts => this.setState({ file: posts }));
+        //タグ一覧取得
+        fetch("http://localhost:4000/tags")
+            .then(response => response.json())
+            .then(posts => this.setState({ defaultTags: posts }));
         //タグ関係取得
 
         //タグ取得
@@ -55,7 +61,16 @@ class ImageDetail extends Component {
 
                     tags:{this.state.tags.map(tag => (
                         <span className="tag">#{tag} </span>
-                    ))}<input placeholder="tags"></input>
+                    ))}
+                    <input type="text" autoComplete="on" list="deftags" onChange={e => this.onChangeAddtag(e)}></input>
+
+                    <datalist id="deftags">
+                        {this.state.defaultTags.map(def => (
+                            <option key={def.id} value={def.name}></option>
+                        ))}
+                    </datalist>
+
+
                     <button onClick={this.addtag}>#</button><br></br>
 
                     <button onClick={this.test} className="buttons">更新</button>
@@ -68,8 +83,21 @@ class ImageDetail extends Component {
         alert("test message");
     }
     addtag() {
+        if (!this.notIntags()) {
+            this.setState({
+                tags: this.state.tags.concat(this.state.newtag)
+            })
+            console.log(this.state.tags);
+        }
+    }
+
+    notIntags() {
+        return this.state.tags.includes(this.state.newtag);
+    }
+
+    onChangeAddtag(e) {
         this.setState({
-            tags: this.state.tags.concat("textTag")
+            newtag: e.target.value
         });
     }
 
